ef97b114297a5f305e37d8be8e4084c86cb3616a
DESKTOP-52QONSF\HP <nastiaklass41062@gmail.com>
2021-11-26 12:55:10 +0200

    Added comments

--- Tasks/Kata.java
+++ Tasks/Kata.java
@@ -1,6 +1,12 @@
 package com.company;
 
 public class Kata {
+
+    //Write a method, that will get an integer array as parameter and will process every number from this array.
+    //Return a new array with processing every number of the input-array
+    //The input array will always contain only positive numbers and will never be empty or null.
+    //The input array should not be modified!
+
     public static int[] squareOrSquareRoot(int[] array) {
         int[] newArray = new int[array.length];
 
